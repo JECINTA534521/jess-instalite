715ebb7c28bbf6ded1b26e43daf820abba6e93a5
jecinta <jecintawanjirug@gmail.com>
2020-03-04 14:54:12 +0300

    this adds the media settings and registers bootstrap4 as an installed app

--- stagram/settings.py
+++ stagram/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tinymce',
     'vote',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
