5d5d4fa3c5a257fb5b9cc47bec692cc0fa094e7f
jecinta <jecintawanjirug@gmail.com>
2020-03-04 12:44:57 +0300

    this adds a a class method to get comment bu id and a class method to save each comment

--- models.py
+++ models.py
@@ -35,4 +35,16 @@ class Comments(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     image = models.ForeignKey(Image,on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image__id=id)
+        return comments
+    
+    def save_comment(self):
+        self.save()
+    
+    def __str__(self):
+        
+        return self.comment
     
