dd0f98f7996adb9dab2157b9e15c575dbe853277
jecinta <jecintawanjirug@gmail.com>
2020-03-04 14:56:42 +0300

    adds a url for viewing someones profile

--- insta/urls.py
+++ insta/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     
     url(r'^$',views.home,name='home'),
     url(r'^image/$', views.image_upload,name='upload'),
-    # url(r'^profile/$', views.profile_info,name='profile'),
-    # url(r'^edit/$',views.profile_edit,name='edit'),
-    # url(r'^new_comment/(\d+)/$' ,views.add_comment,name='newComment'),
-    # url(r'^comment/(\d+)/$' ,views.comments,name='comments'),
-    # url(r'^likes/(\d+)/$' , views.like_images, name='likes'),
-    #  url(r'^user/$',views.search_user,name='search_user')
+    url(r'^profile/$', views.profile_info,name='profile'),
+    url(r'^edit/$',views.profile_edit,name='edit'),
+    url(r'^new_comment/(\d+)/$' ,views.add_comment,name='newComment'),
+    url(r'^comment/(\d+)/$' ,views.comments,name='comments'),
+    url(r'^likes/(\d+)/$' , views.like_images, name='likes'),
+    url(r'^user/$',views.search_user,name='search_user')
 ]
 
 if settings.DEBUG:
