41992707016e6630e9d78fb6ae9c3b051276d877
jecinta <jecintawanjirug@gmail.com>
2020-03-04 14:15:23 +0300

    creates the home vieww function

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    images = Image.objects.all()
+    return render(request, 'insta/index.html',{"images":images})
+
