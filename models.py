0f65e9105270c5bf07240aa5bedf23d9884a56ec
jecinta <jecintawanjirug@gmail.com>
2020-03-04 12:37:13 +0300

    this adds the first Model which is the image model and its properties

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Image(VoteModel,models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=50)
+    image_caption = models.CharField(max_length=50)
+    likes = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
