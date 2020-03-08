6551ff280e680626bc7a1df57222e7b00e6c1911
jecinta <jecintawanjirug@gmail.com>
2020-03-08 19:16:52 +0300

    this adds the follow function to allow users to follow and unfollow other users

--- insta/views.py
+++ insta/views.py
@@ -3,7 +3,7 @@ from . forms import ImageUploadForm,ImageProfileForm,CommentForm
 from .models import *
 from django.contrib.auth.decorators import login_required
 from vote.managers import  VotableManager
-
+from django.contrib.auth.models import User
 votes = VotableManager()
 
 @login_required(login_url='/accounts/login/')
@@ -34,6 +34,7 @@ def profile_info(request):
     current_user=request.user
     profile_info = Profile.objects.filter(user=current_user).first()
     posts =  request.user.image_set.all()
+    
     
     
     return render(request,'insta/profile.html',{"images":posts,"profile":profile_info,"current_user":current_user})
@@ -105,5 +106,41 @@ def search_user(request):
     
    
     
+def unfollow(request, to_unfollow):
+    if request.method == 'GET':
+        user_profile2 = Profile.objects.get(pk=to_unfollow)
+        unfollow_d = Follow.objects.filter(follower=request.user.profile, followed=user_profile2)
+        unfollow_d.delete()
+        return redirect('user_profile', user_profile2.user.username)
+
+
+def follow(request, to_follow):
+    if request.method == 'GET':
+        user_profile3 = Profile.objects.get(pk=to_follow)
+        follow_s = Follow(follower=request.user.profile, followed=user_profile3)
+        follow_s.save()
+        return redirect('user_profile', user_profile3.user.username)
+
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.posts.all()
     
+    followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    params = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    print(followers)
+    return render(request, 'insta/user_profile.html', params)
     
