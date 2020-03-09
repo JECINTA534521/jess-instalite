fd52c7ea698b17f2b28a7950cc3b83e7bff2d630
jecinta <jecintawanjirug@gmail.com>
2020-03-09 12:01:08 +0300

    change the database settings

--- stagram/settings.py
+++ stagram/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'stagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'stagram',
         'USER':'jecinta',
         'PASSWORD':'wanjiru',
